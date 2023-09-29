4583696db280ac06e40544f4047280e151b7625c
Dharvo <adedave77@gmail.com>
2023-09-29 14:12:33 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	modified:   src/Components/Header.tsx 	modified:   src/Components/Navbar.tsx

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const Headers = [
+    "program details",
+    "application form",
+    "workflow",
+    "preview",
+  ];
+  const [active, set] = useState<string>(Headers[1]);
+  return (
+    <header>
+      {Headers?.map((head) => (
+        <div className={`header ${active&&head?'active':''}`}>{head}</div>
+      ))}
+    </header>
+  );
 };
 
 export default Header;
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { MenuOutlined, HomeOutlined, HddOutlined, ContainerOutlined } from "@ant-design/icons";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="Navbar">
+      {/*  MENU */}
+      <MenuOutlined />
+      <div>
+        {/*  HOME */}
+        <HomeOutlined />
+        {/*  LIST */}
+        <HddOutlined/>
+      </div>
+      {/*  USER */}
+      <div className="user">nt</div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
